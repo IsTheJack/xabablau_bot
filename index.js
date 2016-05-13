4f90c9cbd1ddf95bff693b3acc9ec162eca31f9d
Roberto <robertooliveiraestudo@gmail.com>
2016-05-13 12:11:22 -0300

    Jogando pro default caso o comando não exista

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ var commandInvoker = require('./config/commandInvoker')(api);
 api.on('message', function(message) {
     console.log(message);
 
-    if(message.entities && message.entities[0].type === 'bot_command') {
+    if(message.entities && message.entities[0].type === 'bot_command' && !!commandInvoker[message.text]) {
         commandInvoker[message.text](message);
     } else {
         commandInvoker['/start'](message);
